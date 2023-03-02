959a56e807a1fa400d3f23409bd49c4d757b4e07
Sophie <sdillon1@sheffield.ac.uk>
2023-03-02 20:45:46 +0000

    remove line from constructor

--- ssri_project/src/information_processor_model.py
+++ ssri_project/src/information_processor_model.py
@@ -14,7 +14,6 @@ class QuestionModel(QtCore.QAbstractListModel):
         super(QuestionModel, self).__init__(*args, **kwargs)
         self.connection = self.create_connection()
         self.question = question 
-        self.total_dictionary = dict() 
         self.column_list = ["question1", "question2", "question3", "question4", "question5","question6,","question7",
         "question8", "question9", "question10", "question11", "question12", "question13", "question14"]
 
